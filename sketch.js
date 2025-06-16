2d71ae75a52a9f8053d270d3055e3d5a548373eb
nelahryniak <nhryniak@st.swps.edu.pl>
2025-06-16 11:39:59 +0200

    Update sketch.js 1

--- sketch.js
+++ sketch.js
@@ -1,31 +1,46 @@
-let img;
+  appId: "1:687137609047:web:1b186a62144c3e9b969a12",
+    databaseURL:"https://scam-2b8b2-default-rtdb.europe-west1.firebasedatabase.app/"
+};
 
-// Load the image.
-function preload() {
-  img = loadImage('robot.png');
-}
-
-
-function setup() {
-  
-  createCanvas(windowWidth, windowHeight);
-
+  firebase.initializeApp(firebaseConfig);
+ database = firebase.database();
 }
 
 function draw() {
   background(255);
-    fill (0,0)
-rect(windowWidth/3, windowHeight/3,30,50);
+  database.ref("mojaLiczba").on("value", function(snapshot) {
+    liczba = snapshot.val();
+    print("Otrzymano: " + liczba);
+  });
+    database.ref("mojaLiczba2").on("value", function(snapshot) {
+    liczba2 = snapshot.val();
+    print("Otrzymano: " + liczba2);
+  });
+
+  fill(0, 0); 
+  stroke(0); 
+
+  let rectWidth = 30;
+  let rectHeight = 50;
+  let spacing = 10; 
+  let startX = (windowWidth - (11 * rectWidth + 10 * spacing)) / 2; 
+
   
+
+
+
+  let centerX = windowWidth / 2
+  let centerY = windowHeight / 2
+
+  image(img,centerX,centerY+25,200,312);
   
+
   
-  image (img,windowWidth/2,windowHeight/2)
-   img.resize(200, 300);
-  
-  textSize (50)
-  fill(0)
-  textSize (50)
-    textAlign (CENTER,CENTER)
-    text('Czy to twój pesel?',windowWidth/2,windowHeight/5)
-  
+  textSize(40);
+  noStroke();
+  fill(0);
+  textAlign(CENTER, CENTER);
+  text('Czy to twój pesel?', windowWidth / 2, windowHeight / 5);
+text(String(liczba)+String(liczba2),width/2,height/3);
+
 }
